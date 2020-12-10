a2d826f6a9bdacb65bc438336aeabab6299473b5
Yasmine Abdulhamid <yabdulhamid@yasmine-abdulhamid.local>
2020-12-10 17:15:26 -0500

    update

--- project/run_carla.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-SDL_VIDEODRIVER=offscreen /opt/carla-simulator/CarlaUE4.sh -opengl
--- project/run_main.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./solution_cubic_spirals_STARTER/spiral_planner&
-sleep 1
-python3 simulatorAPI.py
