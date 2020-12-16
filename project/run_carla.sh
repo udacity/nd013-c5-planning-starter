bb3b4a8ea94757334abc6cb99ce2e3ddceb05f51
awbrown90 <aaronwbrown@msn.com>
2020-12-16 14:55:59 -0800

    edit run carla script

--- project/run_carla.sh
+++ project/run_carla.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-SDL_VIDEODRIVER=offscreen SDL_HINT_CUDA_DEVICE=0 /opt/carla-simulator/CarlaUE4.sh -opengl&
-sleep 2
-python3 simulatorAPI.py
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+SDL_VIDEODRIVER=offscreen /opt/carla-simulator/CarlaUE4.sh -opengl&
