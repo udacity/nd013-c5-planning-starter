edec4f9d532f65699a0f7d6dfa9cf65bffe847c2
Yasmine A <yabd89@gmail.com>
2020-12-16 17:35:00 -0500

    Update cost_functions.cpp

--- project/solution_cubic_spirals_STARTER/cost_functions.cpp
+++ project/solution_cubic_spirals_STARTER/cost_functions.cpp
@@ -53,8 +53,8 @@ double collision_circles_cost_spiral(const std::vector<PathPoint>& spiral,
       // TODO-Circle placement: Where should the circles be at? The code below
       // is NOT complete. HINT: use CIRCLE_OFFSETS[c], sine and cosine to
       // calculate x and y: cur_y + CIRCLE_OFFSETS[c] * std::sin/cos(cur_yaw)
-      auto circle_center_x = ;  // <- Fix This
-      auto circle_center_y = ;  // <- Fix This
+      auto circle_center_x = 0;  // <- Fix This
+      auto circle_center_y = 0;  // <- Fix This
 
       for (auto obst : obstacles) {
         if (collision) {
@@ -70,7 +70,7 @@ double collision_circles_cost_spiral(const std::vector<PathPoint>& spiral,
           // TODO-Distance from circles to obstacles/actor: How do you calculate
           // the distance between the center of each circle and the
           // obstacle/actor
-          double dist = ;  // <- Fix This
+          double dist = 0;  // <- Fix This
 
           collision = (dist < (CIRCLE_RADII[c] + CIRCLE_RADII[c2]));
         }
@@ -93,9 +93,9 @@ double close_to_main_goal_cost_spiral(const std::vector<PathPoint>& spiral,
   // 1].y and spiral[n - 1].z.
   // Use main_goal.location.x, main_goal.location.y and main_goal.location.z
   // Ex: main_goal.location.x - spiral[n - 1].x
-  auto delta_x = ;  // <- Fix This
-  auto delta_y = ;  // <- Fix This
-  auto delta_z = ;  // <- Fix This
+  auto delta_x = 0;  // <- Fix This
+  auto delta_y = 0;  // <- Fix This
+  auto delta_z = 0;  // <- Fix This
 
   auto dist = std::sqrt((delta_x * delta_x) + (delta_y * delta_y) +
                         (delta_z * delta_z));
